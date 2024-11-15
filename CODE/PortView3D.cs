c8636e54d1bfde81cadf1f3b7bcacb93fd48ff8d
adrian-azan <adrian.azan@hotmail.com>
2024-11-15 10:56:10 -0500

    Camera Refactor
    After adding a 2nd room, it was obvious that the camera was gonna have to work in a couple of different fasions. So I've moved the camera logic to its own class outside of portview so that I can have different cameras
    
    Right now my only camera is the one that pans across the art, but I'll also need some that sit still and rotate. Also made the cameras work with art being anywhere, so the range it pans isnt just hardcoded values

--- CODE/PortView3D.cs
+++ CODE/PortView3D.cs
@@ -1,68 +1,23 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class PortView3D : Node3D
 {
     private Sprite3D _art;
-
-    private Camera3D _camera;
-    private MeshInstance3D _cameraCover;
-    private Timer _fadeTimer;
-    private float _cameraSpeedX;
-    private float _cameraSpeedY;
-    private float _panDuration;
-    private float _zRadius;
-    private Vector3 _cameraStartPosition;
-    private Vector3 _cameraEndPosition;
-
-    private RandomNumberGenerator _rng;
+    private Camera _camera;
 
     public override void _Ready()
     {
-        _art = GetNode<Sprite3D>("Sprite3D");
-        _camera = GetNode<Camera3D>("Camera3D");
-        _cameraCover = GetNode<MeshInstance3D>("Camera3D/MeshInstance3D");
-        _rng = new RandomNumberGenerator();
-
-        _fadeTimer = GetNode<Timer>("Camera3D/Timer");
-
-        SetCamera();
+        _art = GetNode<Sprite3D>("SpotLightRoom/Sprite3D");
+        _camera = GetNode<Camera>("Camera");
+        _camera.Focus(_art);
+        _camera.SetCamera();
     }
 
     public override void _Process(double delta)
     {
-        Vector3 currentPosition = _camera.Position;
-        currentPosition.X -= (float)(_cameraSpeedX * delta);
-        currentPosition.Y -= (float)(_cameraSpeedY * delta);
-        currentPosition.Z = Mathf.Sin((Mathf.Pi) * Mathf.Abs((currentPosition.X - _cameraStartPosition.X) / (_cameraEndPosition.X * 2))) * _zRadius;
-
-        _camera.Position = currentPosition;
-        _camera.LookAt(_art.Position);
-
-        if (Mathf.Abs(_camera.Position.X - _cameraEndPosition.X) <= .65f && _fadeTimer.IsStopped())
-        {
-            (GetNode("Camera3D/MeshInstance3D/AnimationPlayer") as AnimationPlayer).Play("FadeOut");
-            _fadeTimer.Start();
-        }
-    }
-
-    public void SetCamera()
-    {
-        _fadeTimer.Stop();
-        int side = 1;
-        if (_rng.RandiRange(0, 1) == 0)
-            side = -1;
-
-        _camera.Position = new Vector3(_rng.RandiRange(side * 4, side * 8), _rng.RandiRange(8, 14), 0);
-        _cameraEndPosition = new Vector3(-_camera.Position.X, _rng.RandiRange(8, 14), 0);
-        _cameraStartPosition = _camera.Position;
-
-        _panDuration = _rng.RandiRange(7, 20);
-        _zRadius = _rng.RandiRange(1, 8);
-        _cameraSpeedX = (_cameraStartPosition.X - _cameraEndPosition.X) / _panDuration;
-        _cameraSpeedY = Mathf.Abs(_cameraStartPosition.Y - _cameraEndPosition.Y) / _panDuration;
-
-        (GetNode("Camera3D/MeshInstance3D/AnimationPlayer") as AnimationPlayer).Play("FadeIn");
+        _camera._Process(delta, _art);
     }
 
     public void ChangeArt(Texture2D art)
@@ -72,14 +27,14 @@ public partial class PortView3D : Node3D
 
     public void RotateClockwise()
     {
-        Sprite3D art = GetNode("Sprite3D") as Sprite3D;
+        Sprite3D art = GetNode("SpotLightRoom/Sprite3D") as Sprite3D;
 
         art.RotationDegrees = new Vector3(-20, 0, art.RotationDegrees.Z - 90);
     }
 
     public void RotateCounterClockwise()
     {
-        Sprite3D art = GetNode("Sprite3D") as Sprite3D;
+        Sprite3D art = GetNode("SpotLightRoom/Sprite3D") as Sprite3D;
 
         art.RotationDegrees = new Vector3(-20, 0, art.RotationDegrees.Z + 90);
     }
